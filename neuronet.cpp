d5d8e872c242dff3492285c3d8432ea75d74fb00
MaxGsomGsom <max@pochtamt.ru>
2017-04-10 00:22:56 +0300

    init

---
+++ neuronet.cpp
@@ -0,0 +1,7 @@
+#include "neuronet.h"
+
+Neuronet::Neuronet()
+{
+
+}
+
---
+++ neuronet.h
@@ -0,0 +1,11 @@
+#ifndef NEURONET_H
+#define NEURONET_H
+
+
+class Neuronet
+{
+public:
+    Neuronet();
+};
+
+#endif // NEURONET_H
